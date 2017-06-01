57a54dd05a6821b58a01cc6a1f99bcf8f02793dc
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-06-01 14:46:07 -0700

    map without box

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,5 +25,14 @@ $(document).on('turbolinks:load', function (){
 
 	window.addEventListener('scroll', parallax); 
 
+	function initMap() {
+	  var map = new google.maps.Map(document.getElementById('map'), {
+	    zoom: 4,
+	    center: {lat: -33, lng: 151},
+	    disableDefaultUI: true
+	  });
+	}
+
+
 })
 
